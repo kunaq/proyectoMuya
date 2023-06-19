356d355b04af65b14fe0f17a23394f21426ad345
mirellyagv <mirellyagv@gmail.com>
2023-06-19 10:05:40 -0500

    sesion

--- resources/views/components/layouts/nav.blade.php
+++ resources/views/components/layouts/nav.blade.php
@@ -20,12 +20,12 @@
         <li class="nav-item dropdown pe-3">
 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-            <span class="d-none d-md-block dropdown-toggle ps-2" style="color:white">M. Huaman</span>
+            <span class="d-none d-md-block dropdown-toggle ps-2" style="color:white">{{session('nombreTrabajador')}}</span>
           </a><!-- End Profile Iamge Icon -->
 
           <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
             <li class="dropdown-header">
-              <h6>Milagros Huaman</h6>
+              <h6>{{session('nombreTrabajador')}}</h6>
             </li>
             <li>
               <hr class="dropdown-divider">
