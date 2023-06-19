51026797abbdf1fe51d4f8f18aaaa2978b2e6b28
bgalvanguerrero <bgalvanguerrero@gmail.com>
2023-06-19 17:19:36 -0500

    Create ListaController.php

---
+++ app/Http/Controllers/ListaController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7;
+use Psr\Http\Message\ResponseInterface;
+use GuzzleHttp\Exception\RequestException;
+use Illuminate\Support\Facades\Session;
+
+class ListaController extends Controller
+{
+   
+}
