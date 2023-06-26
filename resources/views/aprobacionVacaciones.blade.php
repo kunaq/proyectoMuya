e7a5cad9da53280d9a588fb9faf6219b2ab994b1
jhonamg <orion01123581321@gmail.com>
2023-06-26 13:20:50 -0500

    modal carga masiva

--- resources/views/aprobacionVacaciones.blade.php
+++ resources/views/aprobacionVacaciones.blade.php
@@ -208,41 +208,41 @@
 
 {{-- Indicadores de gestion --}}
 <section class="section dashboard">
-<div class="row">
-    <div class="col-md-10 offset-md-1">
-    <div class="card">
-        <div class="card-body">
-            <h5 class="card-title" style="font-size: 28px;">Indicadores de gestión</h5>
-            <div class="row">
-                <div class="col-md-4" style="margin-bottom: 1rem;">
-                    <div class="card">
-                        <div class="card-body">
-                            <h1 class="card-title tarjeta-vaca-foco"><b>35.8</b></h1>
-                            <h5 style="font-size: 17px; text-align: center;">Promedio de días pendientes de vacaciones del equipo <b>hasta fin de año</b></h5>
+    <div class="row">
+        <div class="col-md-10 offset-md-1">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title" style="font-size: 28px;">Indicadores de gestión</h5>
+                    <div class="row">
+                        <div class="col-md-4" style="margin-bottom: 1rem;">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h1 class="card-title tarjeta-vaca-foco"><b>35.8</b></h1>
+                                    <h5 style="font-size: 17px; text-align: center;">Promedio de días pendientes de vacaciones del equipo <b>hasta fin de año</b></h5>
+                                </div>
+                            </div>
                         </div>
+                        <div class="col-md-4" style="margin-bottom: 1rem;">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h1 class="card-title tarjeta-vaca-foco"><b>23.6</b></h1>
+                                    <h5 style="font-size: 17px;text-align: center;">Promedio de días pendientes de vacaciones del equipo <b>a la fecha de hoy</b></h5>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4" style="margin-bottom: 1rem;">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h1 class="card-title tarjeta-vaca-foco"><b>6</b></h1>
+                                    <h5 style="font-size: 17px;text-align: center;">Número de reprogramaciones realizadas en el año del equipo</h5>
+                                </div>
+                            </div>
+                        </div>                      
                     </div>
                 </div>
-                <div class="col-md-4" style="margin-bottom: 1rem;">
-                    <div class="card">
-                        <div class="card-body">
-                            <h1 class="card-title tarjeta-vaca-foco"><b>23.6</b></h1>
-                            <h5 style="font-size: 17px;text-align: center;">Promedio de días pendientes de vacaciones del equipo <b>a la fecha de hoy</b></h5>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4" style="margin-bottom: 1rem;">
-                    <div class="card">
-                        <div class="card-body">
-                            <h1 class="card-title tarjeta-vaca-foco"><b>6</b></h1>
-                            <h5 style="font-size: 17px;text-align: center;">Número de reprogramaciones realizadas en el año del equipo</h5>
-                        </div>
-                    </div>
-                </div>                      
             </div>
-        </div>
-    </div>
+        </div>  
     </div>  
-</div>  
 </section>
 
 {{-- Reporte de vacaciones --}}
@@ -345,35 +345,19 @@
 <section class="section dashboard">
     <div class="row">
         <div class="col-md-10 offset-md-1">
-            <form class="was-validated" action="{{ route('subirArchivo') }}" method="POST" enctype="multipart/form-data">
-                @csrf
+            {{-- <form class="was-validated" action="{{ route('subirArchivo') }}" method="POST" enctype="multipart/form-data"> --}}
+                {{-- @csrf --}}
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title" style="font-size: 28px;">Cargar vacaciones</h5>
-                        <div class="row">
-                            <div class="col-md-5" style="text-align: -webkit-center">
-                                <input  class="btn btn-success btnDorado" style="padding-right: 0em" type="file" name="archivo">
-                            </div>  
-                            {{-- ----este ejemplo de file con validacion class --}}
-                            {{-- <div class="mb-3">
-                                <input type="file" class="form-control" aria-label="file example" required>
-                                <div class="invalid-feedback">Example invalid form file feedback</div>
-                              </div> --}}
-                            <div class="col-1 col-md-1" style="text-align: -webkit-center; margin: 0em -1em 0em 1em;">
-                                <input class="form-check-input checkVerde" checked type="checkbox" value="" id="flexCheckDefault2">
-                            </div>
-                            <div class="col-11 col-md-4">
-                                <div class="form-group">
-                                    <h5>Aprobar automáticamente las vacaciones que inician el siguiente mes</h5>
-                                </div>
-                            </div>
-                            <div class="col-md-2">
-                                <button class="btn btn-success btnDorado" type="submit" id="buscarDoc">Cargar</button>
+                        <div class="row" style="text-align: center">
+                            <div class="col-12">
+                                <button class="btn btn-success btnDorado" data-bs-toggle="modal" data-bs-target="#ModalCargaMasiva" id="buscarDoc">Carga Masiva</button>
                             </div>                    
                         </div>
                     </div>
                 </div>
-            </form>
+            {{-- </form> --}}
         </div>  
     </div>    
 </section>
@@ -582,6 +566,24 @@ window.onload= function() {
             console.log(e.message);
         }//error
     });//ajax ListarReglasJefe
+
+    $.ajax({
+        url: 'lista/MuestraAnhos', 
+        method: "GET",
+        crossDomain: true,
+        dataType: 'json',
+        success: function(respuesta){ 
+                respuesta['response'].forEach(function(word){
+                //console.log(word);
+                $("#annoIni").append('<option value="'+ word['codvar'] +'">'+ word['desvar1'] +'</option>');
+                // $("#annoFin").append('<option value="'+ word['codvar'] +'">'+ word['desvar1'] +'</option>');
+
+            });
+        },//success
+        error(e){
+            console.log(e.message);
+        }//error
+    });//ajax muestraAnno
 }
 
 //-----------------------Procesar solicitudes de vacaciones---------------------
--- resources/views/components/modals/aprobacionVacaciones.blade.php
+++ resources/views/components/modals/aprobacionVacaciones.blade.php
@@ -15,3 +15,92 @@
       </div>
     </div>
   </div>
+
+  <!-- -------------------Modal Carga Masiva Excel------------------------------- -->
+
+  
+  <form class="" action="{{ route('subirArchivo') }}" method="POST" enctype="multipart/form-data">
+    @csrf
+    
+    
+    <div class="modal fade" id="ModalCargaMasiva" tabindex="-1" aria-labelledby="ModalReglasLabel" aria-hidden="true">
+      <div class="modal-dialog  modal-dialog-centered modal-lg modal-dialog-scrollable">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title card-title fs-5" id="ModalReglasLabel">Carga Masiva de vacaciones</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body" id="">
+            {{-- <div class="col-md-5" style="text-align: -webkit-center">
+                <input  class="btn btn-success btnDorado" type="file" name="archivo">
+            </div> --}}
+
+            
+            
+            <div class="row" style="display: flex; align-items: baseline;">
+              
+              <div class="mb-3 col-12 col-md-6">
+                <div class="form-group">
+                  <label for="annoIni" class="form-label">Año:</label>
+                  <select class="form-select" name="annoIni" id="annoIni">
+                  </select>
+                </div>
+              </div>
+
+              <div class="mb-3 col-12 col-md-6">
+                <div class="form-group">
+                  <label for="periodo" class="form-label">Periodo:</label>
+                  <select class="form-select" name="periodo" id="periodo">
+                    <option value="Enero">Enero</option>
+                    <option value="Febrero">Febrero</option>
+                    <option value="Marzo">Marzo</option>
+                    <option value="Abril">Abril</option>
+                    <option value="Mayo">Mayo</option>
+                    <option value="Junio">Junio</option>
+                    <option value="Julio">Julio</option>
+                    <option value="Agosto">Agosto</option>
+                    <option value="Septiembre">Septiembre</option>
+                    <option value="Octubre">Octubre</option>
+                    <option value="Noviembre">Noviembre</option>
+                    <option value="Diciembre">Diciembre</option>  
+                  </select>              
+                </div>
+              </div>
+
+
+              <div class="col-12 col-md-12 mb-3">
+                <label for="formFile" class="form-label" style="margin-left:5%">Seleccione un archivo Excel con <i class="" data-bs-html="true" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="<p style='text-align: justify;'>La hoja de Excel debe tener 3 columnas ('codigo de trabajador', 'fecha inicio', 'fecha fin') y los datos sin encabezado</p>"><u>formato válido</u></i> y extensión .xls/.xlsx</label>
+                <input type="file" id="archivo" name="archivo" class="form-control" aria-label="file example" style="max-width:70%; margin-left:15%" required>
+                {{-- <div class="invalid-feedback">Example invalid form file feedback</div> --}}
+              </div>
+              <div class="col-12 col-md-2" style="text-align: center;">
+                <input class="form-check-input checkVerde" type="checkbox" value="" id="flexCheckDefault2">
+              </div>
+              <div class="col-12 col-md-10">
+                  <div class="form-group">
+                      <h6>Aprobar automáticamente las vacaciones que inician el siguiente mes</h6>
+                  </div>
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button class="btn btn-success btnDorado" data-bs-dismiss="modal" type="submit" id="buscarDoc2" disabled>Cargar</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </form>
+
+<script>
+  var obj = document.getElementById('archivo');
+
+  obj.addEventListener('input', function(){
+    if (obj.value == '') {
+      document.getElementById('buscarDoc2').setAttribute('disabled','true');
+    }else{
+      document.getElementById('buscarDoc2').removeAttribute('disabled');
+    }
+  });
+  
+</script>
