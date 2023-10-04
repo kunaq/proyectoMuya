ccb665cdf178fcebf62759136b445008f666f86f
mirellyagv <mirellyagv@gmail.com>
2023-10-04 17:01:11 -0500

    tipo de letra

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,8 +8,7 @@
             <div class="card login-card">
                 <div class="row">
                     <div class="col-md-6">
-                        <img src="assets/img/GM-BLANCO.png" alt="login"
-                            class="login-card-img">
+                        <img src="assets/img/GM-BLANCO.png" alt="login" class="login-card-img">
                     </div>
                     <div class="col-md-6">
                         <div class="card-body">
@@ -21,8 +20,7 @@
                             </div>
                             <div class="row banderas">
                                 <div class="form-group">
-                                    <select id="id_select2_example" style="width: 100px;"
-                                        data-minimum-results-for-search="Infinity">
+                                    <select id="id_select2_example" style="width: 100px;" data-minimum-results-for-search="Infinity">
                                         <option data-img_src="assets/img/Peru_bandera.png"></option>
                                         <option data-img_src="assets/img/Ecuador_bandera.png"></option>
                                     </select>
@@ -31,7 +29,7 @@
                             <br>
                             <div class="form-group">
                                 <label for="dni" class="sr-only">Número de DNI</label>
-                                <input type="text" name="dni" id="dni" class="form-control" style="color:black" placeholder="Número del DNI">
+                                <input type="text" name="dni" id="dni" class="form-control" placeholder="Número del DNI">
                             </div>
                             <div class="form-group">
                                 <label for="password" class="sr-only">Clave</label>
@@ -44,8 +42,7 @@
                             </div>
                             <div class="row form-group">
                                 <div class="col-md-6">
-                                    <a href="#" style="text-decoration: underline;" data-bs-toggle="modal"
-                                        data-bs-target="#ModalOlvideC">Olvidé mi contraseña</a>
+                                    <a href="#" style="text-decoration: underline;" data-bs-toggle="modal" data-bs-target="#ModalOlvideC">Olvidé mi contraseña</a>
                                 </div>
                                 <div class="col-md-6">
                                     <div id="message2">
@@ -54,8 +51,7 @@
                                 </div>
                             </div>
                             <div id="message3">
-                                <span style="color: red;">El usuario se encuentra bloqueado temporalmente por
-                                    exceso de intentos fallidos.</span>
+                                <span style="color: red;">El usuario se encuentra bloqueado temporalmente por exceso de intentos fallidos.</span>
                             </div>
                             <div id="message4">
                                 <span style="color: red;">El cargo del usuario no tiene acceso al portal web.</span>
@@ -83,8 +79,7 @@
                                 <div id="helpId" class="form-text" @style('max-width:50%; margin-left: 25%;') hidden>DNI formato inválido</div>
                             </div>
                         </div>
-                        <div class="modal-footer"><button type="submit" id="enviaCorreo" class="btn btn-secondary"
-                                    data-bs-dismiss="modal" disabled="true"> Enviar Correo</button>
+                        <div class="modal-footer"><button type="submit" id="enviaCorreo" class="btn btn-secondary" data-bs-dismiss="modal" disabled="true"> Enviar Correo</button>
                         </div>
                     </form>
                 </div>
@@ -97,10 +92,9 @@
 </x-layouts.index>
 
 {{-- js llamada externa --}}
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity = "sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
 </script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"    integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"  integrity = "sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous">
 </script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.12/dist/sweetalert2.all.min.js"></script>
 
