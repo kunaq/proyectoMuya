beb04da48b697d40ba1210ea9e80aab18bb722cd
mirellyagv <mirellyagv@gmail.com>
2023-10-11 14:54:11 -0500

    Update main.js

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -629,7 +629,7 @@ return meses[mes] || null;
 function esMovil() {
   let navegador = navigator.userAgent;
     if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
-        //console.log("Estás usando un dispositivo móvil!!");
+        alert("Estás usando un dispositivo móvil!!");
         return 'SI';
     } else {
         //console.log("No estás usando un móvil");
